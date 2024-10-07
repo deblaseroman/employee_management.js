7634ae4bba13925246b58d294eaa6d5c567e27ac
roman deblase <rdeblase2000@gmail.com>
2024-10-06 23:05:47 -0400

    create employee class

---
+++ employee_management.js
@@ -0,0 +1,15 @@
+class Employee {
+    constructor(name, salary, position, department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    }
+
+    getDetails() {
+        return `${this.name} - ${this.position}, Salary: $${this.salary}`;
+    }
+}
+
+
+
